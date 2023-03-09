3c272311e2bfa698f54f3de752a4338ebcee7770
Mortularo <111003382+Mortularo@users.noreply.github.com>
2023-03-09 15:50:37 +0500

    HW2
    Добавлен стрелковый монстр. Поворачивается в сторону игрока, стреляет на заданной дистанции.

---
+++ Assets/Scripts/Components/Enemy.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SAM
+{
+    [Serializable]
+    public class Enemy
+    {
+        public float speed = 4f;
+        public float jumpForce = 6f;
+        public float attackRange = 0.5f;
+        public float fireRate = 1f;
+        public float detectingRange = 5f;
+        public int weaponDamage = 5;
+        public int health = 15;
+    }
+}
